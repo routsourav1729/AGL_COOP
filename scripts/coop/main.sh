124fa903cfcf1c087633d222f5a0e03712671af9
routsourav1729 <routsourav1729@gmail.com>
2025-02-24 18:58:27 +0530

    feat: Add experimental UI components

--- scripts/coop/main.sh
+++ scripts/coop/main.sh
@@ -2,7 +2,7 @@
 
 # custom config
 DATA=/home/sourav/ALL_FILES/Thesis/CasPL/data
-TRAINER=AGL
+TRAINER=CoOp  # Keep this as CoOp since we're using coop/main.sh
 
 DATASET=$1
 CFG=$2
@@ -11,12 +11,13 @@ NCTX=$4  # number of context tokens
 SHOTS=$5  # number of shots
 CSC=$6  # class-specific context
 
-for SEED in 1 2 3
+for SEED in 1 #2 3
 do
     DIR=output/${DATASET}/${TRAINER}/${CFG}_${SHOTS}shots/nctx${NCTX}_csc${CSC}_ctp${CTP}/seed${SEED}
     if [ -d "$DIR" ]; then
         echo "Oops! The results exist at ${DIR} (so skip this job)"
     else
+        # The key fix is in how we pass arguments - each override needs proper formatting
         python train.py \
         --root ${DATA} \
         --seed ${SEED} \
@@ -24,9 +25,9 @@ do
         --dataset-config-file configs/datasets/${DATASET}.yaml \
         --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
         --output-dir ${DIR} \
-        TRAINER.COOP.N_CTX ${NCTX} \         # Changed from AGL to COOP
-        TRAINER.COOP.CSC ${CSC} \            # Changed from AGL to COOP
-        TRAINER.COOP.CLASS_TOKEN_POSITION ${CTP} \  # Changed name and namespace
-        DATASET.NUM_SHOTS ${SHOTS}
-        fi
+        TRAINER.COOP.N_CTX "${NCTX}" \
+        TRAINER.COOP.CSC "${CSC}" \
+        TRAINER.COOP.CLASS_TOKEN_POSITION "${CTP}" \
+        DATASET.NUM_SHOTS "${SHOTS}"
+    fi
 done
